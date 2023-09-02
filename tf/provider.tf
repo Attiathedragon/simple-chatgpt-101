80be2cae9bfe0e2b9d132521c5a66a57ebeacce3
Mohamed Attia <85102022+Attiathedragon@users.noreply.github.com>
2023-09-02 12:54:07 +0300

    dd

--- tf/provider.tf
+++ tf/provider.tf
@@ -1,12 +1,10 @@
 variable "access_key" {
   type    = string
-  default = ""
-
+  default = "LTAI5tRdFNSaMA2pwxxyfocM"
 }
 variable "secret_key" {
   type    = string
-  default = ""
-
+  default = "YNl6B82L9wtwFuyYmbc6mbhhovPsfi"
 }
 variable "region" {
   type    = string
