909c52f2e570764b551a1ba67d005ca1d4a2fbe3
Mohamed Attia <mohammed.attia@live.co.uk>
2023-09-03 04:15:32 +0300

    tesst

---
+++ var.tf
@@ -0,0 +1,13 @@
+variable "access_key" {
+  type    = string
+  default = "LTAI5tRdFNSaMA2pwxxyfocM"
+}
+variable "secret_key" {
+  type    = string
+  default = "YNl6B82L9wtwFuyYmbc6mbhhovPsfi"
+}
+variable "region" {
+  type    = string
+  default = "me-central-1"
+}
+#
